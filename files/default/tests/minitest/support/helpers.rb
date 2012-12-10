e7f438c9f2b0c24f4c8fbe6601a5bb34d16cd0cf
Mike Fiedler <miketheman@gmail.com>
2012-12-09 20:13:58 -0500

    style: indentations; esp. case/when blocks

--- files/default/tests/minitest/support/helpers.rb
+++ files/default/tests/minitest/support/helpers.rb
@@ -28,19 +28,23 @@ module Helpers
     end
 
     def apache_service
-      service(case node['platform']
-              when "debian","ubuntu" then "apache2"
-              when "freebsd" then "apache22"
-              else "httpd"
-              end)
+      service(
+        case node['platform']
+        when "debian", "ubuntu" then "apache2"
+        when "freebsd" then "apache22"
+        else "httpd"
+        end
+      )
     end
 
     def config
-      file(case node['platform']
-           when "debian","ubuntu" then "#{node['apache']['dir']}/apache2.conf"
-           when "freebsd" then "#{node['apache']['dir']}/httpd.conf"
-           else "#{node['apache']['dir']}/conf/httpd.conf"
-           end)
+      file(
+        case node['platform']
+        when "debian", "ubuntu" then "#{node['apache']['dir']}/apache2.conf"
+        when "freebsd" then "#{node['apache']['dir']}/httpd.conf"
+        else "#{node['apache']['dir']}/conf/httpd.conf"
+        end
+      )
     end
 
     def ran_recipe?(recipe)
