a920e047b230e289ea8ee5bd807b71ae2b74106c
jtimberman <joshua@opscode.com>
2012-12-12 15:51:42 -0700

    its best to be correct when setting Copyrights

--- recipes/iptables.rb
+++ recipes/iptables.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: apache2
 # Recipe:: iptables
 #
-# Copyright 2008-2009, Opscode, Inc.
+# Copyright 2012, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
