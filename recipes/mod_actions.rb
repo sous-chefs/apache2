9474f9617d252748700651f3dab4b19b647e9080
Igor Rodionov <rodionov.ig@gmail.com>
2013-02-18 14:48:25 +0700

    Update PR

--- recipes/mod_actions.rb
+++ recipes/mod_actions.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: apache2
 # Recipe:: actions 
 #
-# Copyright 2008-2009, Opscode, Inc.
+# Copyright 2013, Igor Rodionov (goruha@gmail.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
