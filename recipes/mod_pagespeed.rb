27159b8e26d87871393316896e6a76e88f39acd2
Chris Dwan <radixhound@gmail.com>
2013-03-11 16:18:52 -0700

    Use file_cache_path instead of /tmp/

--- recipes/mod_pagespeed.rb
+++ recipes/mod_pagespeed.rb
@@ -1,12 +1,12 @@
 if platform?("debian", "ubuntu")
-  remote_file "/tmp/mod-pagespeed.deb" do
+  remote_file "#{Chef::Config[:file_cache_path]}/mod-pagespeed.deb" do
     source "https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb"
     mode 0644
     action :create_if_missing
   end
 
   dpkg_package "mod_pagespeed" do
-    source "/tmp/mod-pagespeed.deb"
+    source "#{Chef::Config[:file_cache_path]}/mod-pagespeed.deb"
     action :install
   end
   
