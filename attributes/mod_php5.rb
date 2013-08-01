36e53a82d41a3784befa3408547189a4ec472bd8
Lucas Hansen <lucash@opscode.com>
2013-08-01 14:20:36 -0700

    Finished adding Windows support for mod_php.

---
+++ attributes/mod_php5.rb
@@ -0,0 +1,4 @@
+default["apache"]["mod_php5"]["windows"]["source"] = "http://windows.php.net/downloads/releases/php-5.3.27-Win32-VC9-x86.zip"
+default["apache"]["mod_php5"]["windows"]["file"] = "php5apache2_2.dll"
+default["apache"]["mod_php5"]["windows"]["checksum"] = "588c7102d7570da5e9efdf3a84028a2ec13afbc49b57bb72128841cfc9a561d3"
+default["apache"]["mod_php5"]["windows"]["php_conf_dir"] = "C:/PHP"
