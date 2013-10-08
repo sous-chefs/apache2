124fc1ceff08e4c86ec952dd964fe5abc680e943
Mark Butcher <mbutcher@idondemand.com>
2013-10-08 19:23:30 +1100

    [COOK-3757] Provide missing variable to ports.conf
    The ports.conf.erb template requires apache_listen_addresses this is not
    done by the mod_ssl recipe (though default does this correctly).

--- recipes/mod_ssl.rb
+++ recipes/mod_ssl.rb
@@ -36,7 +36,10 @@ end
 template "#{node['apache']['dir']}/ports.conf" do
   source    'ports.conf.erb'
   mode      '0644'
-  variables(:apache_listen_ports => ports.map { |p| p.to_i }.uniq)
+  variables(
+    :apache_listen_addresses => node['apache']['listen_addresses'].uniq,
+    :apache_listen_ports => node['apache']['listen_ports'].map { |p| p.to_i }.uniq
+  )
   notifies  :restart, 'service[apache2]'
 end
 
