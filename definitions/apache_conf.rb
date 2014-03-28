a7839526670f3101a1e995f58ba019449264ebfb
Steven Barre <steven@stevenbarre.com>
2014-03-28 00:21:03 -0400

    [COOK-4469] - Use reload instead of restart on RHEL
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- definitions/apache_conf.rb
+++ definitions/apache_conf.rb
@@ -21,6 +21,6 @@ define :apache_conf do
   template "#{node['apache']['dir']}/mods-available/#{params[:name]}.conf" do
     source   "mods/#{params[:name]}.conf.erb"
     mode     '0644'
-    notifies :restart, 'service[apache2]'
+    notifies :reload, 'service[apache2]'
   end
 end
