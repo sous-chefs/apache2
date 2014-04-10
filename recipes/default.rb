1fdeb238406cdc86cb4d8188bc4e55844b87de71
Jean Mertz <jean@mertz.fm>
2014-04-09 22:03:28 -0400

    [COOK-4492] - Fix service[apache2] CHEF-3694 duplication
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,28 +21,6 @@ package 'apache2' do
   package_name node['apache']['package']
 end
 
-service 'apache2' do
-  case node['platform_family']
-  when 'rhel', 'fedora', 'suse'
-    service_name 'httpd'
-    # If restarted/reloaded too quickly httpd has a habit of failing.
-    # This may happen with multiple recipes notifying apache to restart - like
-    # during the initial bootstrap.
-    restart_command '/sbin/service httpd restart && sleep 1'
-    reload_command '/sbin/service httpd reload && sleep 1'
-  when 'debian'
-    service_name 'apache2'
-    restart_command '/usr/sbin/invoke-rc.d apache2 restart && sleep 1'
-    reload_command '/usr/sbin/invoke-rc.d apache2 reload && sleep 1'
-  when 'arch'
-    service_name 'httpd'
-  when 'freebsd'
-    service_name 'apache22'
-  end
-  supports [:restart, :reload, :status]
-  action :enable
-end
-
 if platform_family?('rhel', 'fedora', 'arch', 'suse', 'freebsd')
   directory node['apache']['log_dir'] do
     mode '0755'
@@ -208,5 +186,23 @@ apache_site 'default' do
 end
 
 service 'apache2' do
-  action :start
+  case node['platform_family']
+  when 'rhel', 'fedora', 'suse'
+    service_name 'httpd'
+    # If restarted/reloaded too quickly httpd has a habit of failing.
+    # This may happen with multiple recipes notifying apache to restart - like
+    # during the initial bootstrap.
+    restart_command '/sbin/service httpd restart && sleep 1'
+    reload_command '/sbin/service httpd reload && sleep 1'
+  when 'debian'
+    service_name 'apache2'
+    restart_command '/usr/sbin/invoke-rc.d apache2 restart && sleep 1'
+    reload_command '/usr/sbin/invoke-rc.d apache2 reload && sleep 1'
+  when 'arch'
+    service_name 'httpd'
+  when 'freebsd'
+    service_name 'apache22'
+  end
+  supports [:restart, :reload, :status]
+  action [:enable, :start]
 end
