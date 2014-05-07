0e976e5cc859cf995a463a0c8919ca8fecc131ff
Philip Hutchins <flipture@gmail.com>
2014-05-07 08:13:52 -0400

    made changes for Ubuntu 14.04 compatibility

--- attributes/default.rb
+++ attributes/default.rb
@@ -177,9 +177,12 @@ default['apache']['proxy']['allow_from'] = 'none'
 # Default modules to enable via include_recipe
 
 default['apache']['default_modules'] = %w[
-  status alias auth_basic authn_file authz_default authz_groupfile authz_host authz_user autoindex
+  status alias auth_basic authn_file authz_groupfile authz_host authz_user autoindex
   dir env mime negotiation setenvif
 ]
+if !%w[trusty].include?(node['lsb']['codename']])
+  default['apache']['default_modules'].push("authz_default")
+end
 
 %w[log_config logio].each do |log_mod|
   default['apache']['default_modules'] << log_mod if %w[rhel fedora suse arch freebsd].include?(node['platform_family'])
