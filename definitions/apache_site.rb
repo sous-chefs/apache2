37629fbccd7c91c588c36d9994eb846164398b1f
Taylor Price <tprice@onehealth.com>
2014-06-23 11:38:50 -0700

    adding .conf to file checks

--- definitions/apache_site.rb
+++ definitions/apache_site.rb
@@ -25,18 +25,18 @@ define :apache_site, :enable => true do
       command "/usr/sbin/a2ensite #{params[:name]}"
       notifies :restart, 'service[apache2]'
       not_if do
-        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/#{params[:name]}") ||
-        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/000-#{params[:name]}")
+        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/#{params[:name]}.conf") ||
+        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/000-#{params[:name]}.conf")
       end
-      only_if { ::File.exists?("#{node['apache']['dir']}/sites-available/#{params[:name]}") }
+      only_if { ::File.exist?("#{node['apache']['dir']}/sites-available/#{params[:name]}.conf") }
     end
   else
     execute "a2dissite #{params[:name]}" do
       command "/usr/sbin/a2dissite #{params[:name]}"
       notifies :restart, 'service[apache2]'
       only_if do
-        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/#{params[:name]}") ||
-        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/000-#{params[:name]}")
+        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/#{params[:name]}.conf") ||
+        ::File.symlink?("#{node['apache']['dir']}/sites-enabled/000-#{params[:name]}.conf")
       end
     end
   end
