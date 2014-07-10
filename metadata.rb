b05b9d5bc83e12ba301be53dd567ad5b9cd99018
cheftony <cheftony16@gmail.com>
2014-07-10 16:47:10 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email  "webops@lapresse.ca"
 license           "Apache 2.0"
 description       "Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.6.5"
+version           "1.6.6"
 recipe            "apache2", "Main Apache configuration"
 recipe            "apache2::logrotate", "Rotate apache2 logs. Requires logrotate cookbook"
 recipe            "apache2::mod_alias", "Apache module 'alias' with config file"
