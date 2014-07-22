3f416d0cc8ed800e25c9e030b955a2f16540cf45
Taylor Price <tprice@onehealth.com>
2014-07-22 13:21:12 -0700

    readding sysconf_additional_params that keeps getting removed

--- attributes/default.rb
+++ attributes/default.rb
@@ -166,6 +166,7 @@ default['apache']['timeout']           = 300
 default['apache']['keepalive']         = 'On'
 default['apache']['keepaliverequests'] = 100
 default['apache']['keepalivetimeout']  = 5
+default['apache']['sysconfig_additional_params'] = {}
 
 # Security
 default['apache']['servertokens']    = 'Prod'
