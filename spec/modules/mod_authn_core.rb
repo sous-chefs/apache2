4a18d8e7a99815d7e21ca0b958b92f2121305513
Taylor Price <tprice@onehealth.com>
2014-07-24 11:39:19 -0700

    working test for writing to log in 2.2

--- spec/modules/mod_authn_core.rb
+++ spec/modules/mod_authn_core.rb
@@ -1,19 +1,22 @@
 require 'spec_helper'
 
 describe 'apache2::mod_authn_core' do
-
   supported_platforms.each do |platform, versions|
     versions.each do |version|
       context "on #{platform.capitalize} #{version}" do
         let(:chef_run) do
-          ChefSpec::Runner.new(:platform => platform, :version => version).converge(described_recipe)
+          ChefSpec::Runner.new(:platform => platform, :version => version) do
+          end.converge(described_recipe)
         end
-        if platform[:apache][:version] == '2.2'
-          expect(chef_run).to write_log('Ignoring apache2::mod_authn_core. not available until apache 2.4')
+        property = load_platform_properties(:platform => platform, :platform_version => version)
+        if property[:apache][:version] == '2.2'
+          it 'writes to the log' do
+            expect(chef_run).to write_log('Ignoring apache2::mod_authn_core. not available until apache 2.4')
+          end
+        elsif property[:apache][:version] == '2.4'
+          # it_should_behave_like 'an apache2 module', 'authn_core', false, supported_platforms
         end
       end
     end
   end
-
-  it_should_behave_like 'an apache2 module', 'authn_core', false, supported_platforms, 'mod_authn_core.so'
 end
