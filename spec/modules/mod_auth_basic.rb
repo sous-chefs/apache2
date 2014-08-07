17712b8aa8ce8b8effec82e2b88eeb0660f94658
BK Box <bk.box@rackspace.com>
2014-08-07 09:25:13 -0500

    Fix tests

--- spec/modules/mod_auth_basic.rb
+++ spec/modules/mod_auth_basic.rb
@@ -1,18 +1,8 @@
 require 'spec_helper'
 
-platforms = {
-  'ubuntu' => ['12.04', '14.04'],
-  'debian' => ['7.0', '7.4'],
-  'fedora' => %w(18 20),
-  'redhat' => ['5.9', '6.5'],
-  'centos' => ['5.9', '6.5'],
-  'freebsd' => ['9.2'],
-  'suse' => ['11.3']
-}
-#  'arch' =>
-
 describe 'apache2::mod_auth_basic' do
-  platforms.each do |platform, versions|
+  it_should_behave_like 'an apache2 module', 'fcgid', true, supported_platforms
+  supported_platforms.each do |platform, versions|
     versions.each do |version|
       context "on #{platform.capitalize} #{version}" do
         let(:chef_run) do
@@ -26,6 +16,4 @@ describe 'apache2::mod_auth_basic' do
       end
     end
   end
-
-  it_should_behave_like 'an apache2 module', 'fcgid', true, platforms
 end
--- spec/modules/mod_fcgid_spec.rb
+++ spec/modules/mod_fcgid_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe 'apache2::mod_fcgid' do
+  before do
+    stub_command('/usr/sbin/httpd -t').and_return('Syntax OK')
+  end
+
   it_should_behave_like 'an apache2 module', 'fcgid', true, supported_platforms
   supported_platforms.each do |platform, versions|
     versions.each do |version|
@@ -12,7 +16,7 @@ describe 'apache2::mod_fcgid' do
 
         it 'includes yum-epel' do
           expect(chef_run).to include_recipe('yum-epel')
-        end if %w(redhat centos fedora).include?(platform)
+        end if %w(redhat centos).include?(platform)
       end
     end
   end
