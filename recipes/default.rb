6c4bf7747685d2949c248746735c395897ead0f1
Martin Smith <martin@mbs3.org>
2014-08-28 12:23:21 -0500

    Use correct configtest, don't override provider
    Per https://github.com/onehealth-cookbooks/apache2/issues/189.

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,14 +26,12 @@ service 'apache2' do
   case node['platform_family']
   when 'rhel'
     reload_command '/sbin/service httpd graceful'
-  when 'debian'
-    provider Chef::Provider::Service::Debian
   when 'arch'
     service_name 'httpd'
   end
   supports [:start, :restart, :reload, :status]
   action [:enable, :start]
-  only_if "#{node['apache']['binary']} -t", :environment => { 'APACHE_LOG_DIR' => node['apache']['log_dir'] }, :timeout => 10
+  only_if "#{node['apache']['apachectl']} -t", :timeout => 10
 end
 
 %w(sites-available sites-enabled mods-available mods-enabled conf-available conf-enabled).each do |dir|
