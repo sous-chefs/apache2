f749408cd2dbc738c8a2c771513155d892005b09
Maks3w <github.maks3w@virtualplanets.net>
2014-10-17 19:22:57 +0200

    openid fix

--- spec/modules/mod_auth_openid_spec.rb
+++ spec/modules/mod_auth_openid_spec.rb
@@ -1,24 +1,21 @@
 require 'spec_helper'
 
 describe 'apache2::mod_auth_openid' do
-  before do
-    # node['apache']['mod_auth_openid']['version']
-    version = '0.8'
-    stub_command("test -f #{Chef::Config[:file_cache_path]}/mod_auth_openid-#{version}/src/.libs/mod_auth_openid.so").and_return(true)
-  end
-
   supported_platforms.each do |platform, versions|
     versions.each do |version|
       context "on #{platform.capitalize} #{version}" do
         let(:chef_run) do
-          ChefSpec::SoloRunner.new(:platform => platform, :version => version).converge(described_recipe)
+          @chef_run
         end
 
         property = load_platform_properties(:platform => platform, :platform_version => version)
 
         before do
+          @chef_run = ChefSpec::SoloRunner.new(:platform => platform, :version => version)
+          stub_command("test -f #{Chef::Config[:file_cache_path]}/mod_auth_openid-#{property[:apache][:mod_auth_openid][:version]}/src/.libs/mod_auth_openid.so").and_return(true)
           stub_command("test -f #{property[:apache][:libexec_dir]}/mod_auth_openid.so").and_return(true)
           stub_command("#{property[:apache][:binary]} -t").and_return(true)
+          chef_run.converge(described_recipe)
         end
 
         if %w(debian ubuntu suse opensuse).include?(platform)
