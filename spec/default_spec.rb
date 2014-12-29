f08e96bb7e3e2734ea94828bd5fb9963ed5fe511
dann7387 <danny.cheung@rackspace.com>
2014-12-29 10:34:27 +0000

    Revert "Remove blank lines to resolve rubocop errors."
    This reverts commit 12b6000090556829db2faef3659c6543c722a050.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -66,6 +66,7 @@ describe 'apache2::default' do
           end
 
           %w(a2ensite a2dissite a2enmod a2dismod a2enconf a2disconf).each do |modscript|
+
             it "creates /usr/sbin/#{modscript}" do
               expect(chef_run).to create_template("/usr/sbin/#{modscript}")
             end
