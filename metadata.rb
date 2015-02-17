086c0c181c322f67fba6676c630c66118ac7c5ee
Sander van Zoest <sander@vanzoest.com>
2015-02-16 19:30:25 -0800

    Update maintainer metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'apache2'
-maintainer 'OneHealth Solutions, Inc.'
-maintainer_email 'cookbooks@onehealth.com'
+maintainer 'Sander van Zoest'
+maintainer_email 'sander+cookbooks@vanzoest.com'
 license 'Apache 2.0'
 description 'Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
