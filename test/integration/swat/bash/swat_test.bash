82edf81a4130c236a628a65bf3b23603d5f5375b
Alexey Melezhik <melezhik@gmail.com>
2016-01-14 11:27:40 +0300

    landing-page-string

--- test/integration/swat/bash/swat_test.bash
+++ test/integration/swat/bash/swat_test.bash
@@ -1,4 +1,4 @@
-yum install perl-Test-Harness -y -q
+yum install perl-Test-Harness git -y -q
 
 which cpanm 1>/dev/null || curl -k -L http://cpanmin.us/ -o /usr/bin/cpanm
 
@@ -13,17 +13,21 @@ cpanm Sparrow || exit 1
 which sparrow 1>/dev/null || exit 1
 
 # sparrow plg remove swat-apache2-cookbook
+echo 'swat-apache2-cookbook https://github.com/melezhik/swat-apache2-cookbook.git' > ~/sparrow.list
+
 sparrow index update
 
-sparrow plg install swat-apache2-cookbook
+sparrow plg install private@swat-apache2-cookbook
 
 sparrow project create foo
 
 sparrow check add foo apache
 
-sparrow check set foo apache -p swat-apache2-cookbook -u `hostname -f`
+sparrow check set foo apache -p private@swat-apache2-cookbook -u `hostname -f`
 
-match_l=300 sparrow check run foo apache
+landing_page_line='Hello World' \
+match_l=300 \
+sparrow check run foo apache
 
 st=$?
 
