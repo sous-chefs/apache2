314fd991070319ae1028df749cd91fc693d3c280
Sander van Zoest <sander@vanzoest.com>
2016-03-25 11:43:01 -0700

    Update platform fixtures and chefspec supported platforms

--- spec/support/supported_platforms.rb
+++ spec/support/supported_platforms.rb
@@ -3,10 +3,10 @@ def supported_platforms
   platforms = {
     'amazon' => ['2014.09'],
     'ubuntu' => ['14.04'],
-    'debian' => ['7.6'],
-    'fedora' => %w(18 21),
+    'debian' => ['7.9'],
+    'fedora' => %w(23),
     'redhat' => ['7.0'],
-    'centos' => ['6.5'],
+    'centos' => ['6.7'],
     'freebsd' => ['10.0'],
     'opensuse' => ['13.1']
   }
