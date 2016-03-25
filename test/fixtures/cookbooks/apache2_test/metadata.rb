a508058eb23875a7c41310b3ef6a7ce25e7d8450
Sander van Zoest <sander@vanzoest.com>
2016-03-25 11:26:38 -0700

    Remove attributes from apache2_test metadata.rb

--- test/fixtures/cookbooks/apache2_test/metadata.rb
+++ test/fixtures/cookbooks/apache2_test/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrew@kotirisoftware.com'
 license          'Apache 2.0'
 description      'Acceptance tests for apache2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'apache2'
 depends          'jpackage'
@@ -34,56 +34,3 @@ recipe           'apache2_test::mod_status_remote', 'Test example for viewing se
 %w{centos ubuntu}.each do |os|
   supports os
 end
-
-attribute 'apache_test/auth_username',
-          :display_name => 'Test Username',
-          :description => 'Username for the test user',
-          :default => 'bork'
-
-attribute 'apache_test/auth_password',
-          :display_name => 'Test Password',
-          :description => 'Password for the test user',
-          :default => 'secret'
-
-attribute 'apache_test/cache_expiry_seconds',
-          :display_name => 'Cache Expiry (Seconds)',
-          :description => 'The expiry time to set in caching response headers',
-          :default => '60'
-
-attribute 'apache_test/app_dir',
-          :display_name => 'Application Directory',
-          :description => 'Parent directory to deploy test applications under',
-          :default => '/home/apache2/env'
-
-attribute 'apache_test/cgi_dir',
-          :display_name => 'CGI Directory',
-          :description => 'Directory to install CGI scripts into',
-          :default => '/usr/lib/cgi-bin'
-
-attribute 'apache_test/root_dir',
-          :display_name => 'Root Directory',
-          :description => 'Webserver document root directory',
-          :default => '/var/www'
-
-attribute 'apache_test/remote_host_ip',
-          :display_name => 'Remote Host IP',
-          :description => 'IP Address to allow requests from',
-          :default => '192.168'
-
-attribute 'apache_test/ssl_dir',
-          :display_name => 'SSL Directory',
-          :description => 'Directory for SSL certificates',
-          :default => '/home/apache2'
-
-attribute 'apache_test/ssl_cert_file',
-          :display_name => 'SSL Certificate Path',
-          :description => 'File path for the generated self-signed certificate'
-
-attribute 'apache_test/ssl_cert_key_file',
-          :display_name => 'SSL Certificate Private Key',
-          :description => 'File path for the generated private key'
-
-attribute 'apache_test/svn_dir',
-          :display_name => 'Subversion Directory',
-          :description => 'File path for test Subversion repository',
-          :default => '/home/apache2/svn'
