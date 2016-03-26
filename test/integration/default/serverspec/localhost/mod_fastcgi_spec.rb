fe32732e1b2a8d27ca6be8317d6c5fe7675a027a
Sander van Zoest <sander@vanzoest.com>
2016-03-25 23:11:54 -0700

    Remove mod_fastcgi from tests #415

--- test/integration/default/serverspec/localhost/mod_fastcgi_spec.rb
+++ test/integration/default/serverspec/localhost/mod_fastcgi_spec.rb
@@ -19,19 +19,19 @@ require "#{ENV['BUSSER_ROOT']}/../kitchen/data/serverspec_helper"
 describe 'apache2::mod_fastcgi' do
   expected_module = 'fastcgi'
   subject(:available) { file("#{property[:apache][:dir]}/mods-available/#{expected_module}.load") }
-  it "mods-available/#{expected_module}.load is accurate" do
+  xit "mods-available/#{expected_module}.load is accurate" do
     expect(available).to be_file
     expect(available).to be_mode 644
     expect(available.content).to match "LoadModule #{expected_module}_module #{property[:apache][:libexec_dir]}/mod_#{expected_module}.so\n"
   end
 
   subject(:enabled) { file("#{property[:apache][:dir]}/mods-enabled/#{expected_module}.load") }
-  it "mods-enabled/#{expected_module}.load is a symlink to mods-available/#{expected_module}.load" do
+  xit "mods-enabled/#{expected_module}.load is a symlink to mods-available/#{expected_module}.load" do
     expect(enabled).to be_linked_to("../mods-available/#{expected_module}.load")
   end
 
   subject(:loaded_modules) { command("APACHE_LOG_DIR=#{property[:apache][:log_dir]} #{property[:apache][:binary]} -M") }
-  it "#{expected_module} is loaded" do
+  xit "#{expected_module} is loaded" do
     expect(loaded_modules.exit_status).to eq 0
     expect(loaded_modules.stdout).to match(/#{expected_module}_module/)
   end
