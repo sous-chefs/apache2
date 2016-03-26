0e1d1a129bb288e19f52b22e591af8753158a1db
Sander van Zoest <sander@vanzoest.com>
2016-03-26 14:45:53 -0700

    bump revision for mod_auth_cas source to v1.0.9.1

--- attributes/mod_auth_cas.rb
+++ attributes/mod_auth_cas.rb
@@ -18,4 +18,4 @@
 #
 
 default['apache']['mod_auth_cas']['from_source']     = false
-default['apache']['mod_auth_cas']['source_revision'] = 'v1.0.8.1'
+default['apache']['mod_auth_cas']['source_revision'] = 'v1.0.9.1'
