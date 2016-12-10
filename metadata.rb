fdc6685966a4b132f3b5071df0e8b3a0b6350fde
Sander van Zoest <sander@vanzoest.com>
2016-12-10 15:46:57 -0800

    Chef Brigade -> Sous Chefs

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'apache2'
-maintainer 'Chef Brigade'
-maintainer_email 'help@chefbrigade.io'
-issues_url 'https://github.com/sous-chefs/apache2/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/sous-chefs/apache2/' if respond_to?(:source_url)
+source_url "https://github.com/sous-chefs/apache2" if respond_to?(:source_url)
+issues_url "https://github.com/sous-chefs/apache2/issues" if respond_to?(:issues_url)
+maintainer 'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
 chef_version '>= 11' if respond_to?(:chef_version)
 license 'Apache 2.0'
 description 'Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions'
