b2814482b6e4c11cf9e1f338608ce47f55802cd6
Tim Smith <tsmith@chef.io>
2017-03-04 11:49:07 -0800

    Update copyright header format
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/apache2_test/recipes/mod_perl.rb
+++ test/cookbooks/apache2_test/recipes/mod_perl.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: apache2_test
+# Cookbook:: apache2_test
 # Recipe:: mod_perl
 #
-# Copyright 2012, Chef Software, Inc.
+# Copyright:: 2012, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
