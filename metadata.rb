47a7449c59004a836ca84553890ccc7656372968
Dan Webb <dan.webb@damacus.io>
2017-07-06 19:36:47 +0100

    Version bump to 3.3.1

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ chef_version '>= 11' if respond_to?(:chef_version)
 license 'Apache-2.0'
 description 'Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.3.0'
+version '3.3.1'
 
 supports 'debian'
 supports 'ubuntu'
