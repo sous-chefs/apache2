0193dd5600b541f0a1ceebc7f2dfcbddf8f11627
Tim Smith <tsmith@chef.io>
2017-07-07 23:06:42 -0700

    Test on Debian 9
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/apache2_test/recipes/setup.rb
+++ test/cookbooks/apache2_test/recipes/setup.rb
@@ -2,9 +2,9 @@ apt_update 'update'
 
 case node['platform_family']
 when 'debian'
-  package %w(libxml2 libxml2-dev libxslt1-dev)
+  package %w(libxml2 libxml2-dev libxslt1-dev net-tools)
 when 'rhel'
-  package %w(gcc make ruby-devel libxml2 libxml2-devel libxslt libxslt-devel)
+  package %w(gcc make ruby-devel libxml2 libxml2-devel libxslt libxslt-devel net-tools)
 end
 
 package 'curl'
