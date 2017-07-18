13ae3a54ea44dad7e8fa2b226ff19bed048fd399
Jean-Bernard DAMIANO <jbdamiano@gmail.com>
2017-07-18 19:07:07 +0200

    Update mod_ldap.rb
    fix coding style

--- recipes/mod_ldap.rb
+++ recipes/mod_ldap.rb
@@ -16,9 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-if platform_family?('rhel', 'fedora', 'amazon')
-  package 'mod_ldap'
-end
+package 'mod_ldap' if platform_family?('rhel', 'fedora', 'amazon')
 
 apache_module 'ldap' do
   conf true
