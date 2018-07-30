560d83d1de60baf283627b30850af5f3029c084b
Dan Webb <dan.webb@damacus.io>
2018-07-30 21:06:03 +0100

    Add service_name helper and unit tests
    - Adds service_name helper
    - Remove some cases from the apache_binary helper

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -3,16 +3,23 @@ module Apache2
     module Helpers
       def apache_binary
         case node['platform_family']
-        when 'rhel', 'fedora', 'amazon', 'arch'
+        when 'rhel', 'fedora', 'amazon', 'arch', 'suse'
           '/usr/sbin/httpd'
-        when 'suse'
-          '/usr/sbin/httpd'
-        when 'debian'
-          '/usr/sbin/apache2'
         when 'freebsd'
           '/usr/local/sbin/httpd'
         else
           '/usr/sbin/apache2'
+        end
+      end
+
+      def service_name
+        case node['platform_family']
+        when 'rhel', 'fedora', 'amazon', 'arch'
+          'httpd'
+        when 'freebsd'
+          'apache24'
+        else
+          'apache2'
         end
       end
     end
