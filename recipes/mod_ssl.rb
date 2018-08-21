61a1ef4dcaa8d6b55910d1c6db3241a957c842b8
Ryan Skadberg <rskadberg@patientslikeme.com>
2018-08-21 17:31:12 -0400

    Fix ports.conf location and how its set up (#550)
    * Fix ports.conf location and how its set up

--- recipes/mod_ssl.rb
+++ recipes/mod_ssl.rb
@@ -16,8 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 if node['apache']['listen'] == ['*:80']
-  node.default['apache']['listen'] = ['*:80', "*:#{node['apache']['mod_ssl']['port']}"]
+  node.default['apache']['listen'] += ["*:#{node['apache']['mod_ssl']['port']}"]
 end
 
 include_recipe 'apache2::default'
@@ -34,11 +35,8 @@ if platform_family?('rhel', 'fedora', 'suse', 'amazon')
   end
 end
 
-template 'ssl_ports.conf' do
-  path "#{apache_dir}/ports.conf"
-  source 'ports.conf.erb'
-  mode '0644'
-  notifies :restart, 'service[apache2]', :delayed
+apache_module 'ports' do
+  conf true
 end
 
 apache_module 'ssl' do
