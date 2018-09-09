b0e8d53eba9b7413d8fcf02ca647c82eb7a7f15f
Dan Webb <dan.webb@damacus.io>
2018-09-09 13:03:21 +0100

    Working lib_dir unit harness. Now to get some more failing test!

--- spec/libraries/lib_dir_spec.rb
+++ spec/libraries/lib_dir_spec.rb
@@ -3,27 +3,48 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require_relative '../../libraries/helpers'
 
-RSpec.describe Apache2::Cookbook::Helpers do
-  describe "#lib_dir_for_machine" do
-    context 'x86_64' do
+describe "#lib_dir_for_machine" do
+  context 'x86_64' do
+    automatic_attributes["kernel"]["machine"] = "x86_64"
+
+    context 'redhat' do
       recipe do
         log lib_dir_for_machine
       end
 
-      context 'redhat' do
-        platform 'redhat'
-        it { is_expected.to write_log("/usr/lib64") }
+      platform 'redhat'
+      it { is_expected.to write_log("/usr/lib64") }
+    end
+
+    context 'arch' do
+      recipe do
+        log lib_dir_for_machine
       end
 
-      context 'arch' do
-        platform 'arch'
-        it { is_expected.to write_log("/usr/lib") }
+      platform 'arch'
+      it { is_expected.to write_log("/usr/lib") }
+    end
+
+    context 'freebsd' do
+      recipe do
+        log lib_dir_for_machine
       end
 
-      context 'freebsd' do
-        platform 'freebsd'
-        it { is_expected.to write_log("/usr/local") }
+      platform 'freebsd'
+      it { is_expected.to write_log("/usr/local") }
+    end
+  end
+
+  context 'x86'do
+    automatic_attributes["kernel"]["machine"] = "i686"
+
+    context 'redhat' do
+      recipe do
+        log lib_dir_for_machine
       end
+
+      platform 'redhat'
+      it { is_expected.to write_log("/usr/lib") }
     end
   end
 end
