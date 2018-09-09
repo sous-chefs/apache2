4250bbac3f6c85383e9fcfa3b275739b68e4f747
Dan Webb <dan.webb@damacus.io>
2018-09-09 12:00:56 +0100

    Use new chefspec matcher

--- spec/libraries/lib_dir_spec.rb
+++ spec/libraries/lib_dir_spec.rb
@@ -1,98 +1,29 @@
 require 'spec_helper'
 require 'chefspec'
+require 'chefspec/berkshelf'
 require_relative '../../libraries/helpers'
 
 RSpec.describe Apache2::Cookbook::Helpers do
-  class DummyClass < Chef::Node
-    include Apache2::Cookbook::Helpers
-  end
+  describe "#lib_dir_for_machine" do
+    context 'x86_64' do
+      recipe do
+        log lib_dir_for_machine
+      end
 
-  subject { DummyClass.new }
+      context 'redhat' do
+        platform 'redhat'
+        it { is_expected.to write_log("/usr/lib64") }
+      end
 
-  describe '#lib_dir_for_machine' do
-    before do
-      allow(subject).to receive(:[]).and_return({kernel: {machine: 'x64'} } )
+      context 'arch' do
+        platform 'arch'
+        it { is_expected.to write_log("/usr/lib") }
+      end
 
-      # allow(myobject).to receive(:node).and_return({kernel: {machine: ‘fake’}, other: ‘attribute’})
-
-    end
-
-    context 'with a 64 bit machine' do
-      # let(:kernel) { machine: 's390x' }
-      # let(:platform_family) { 'rhel' }
-
-      it 'returns the correct directory' do
-        expect(subject.lib_dir_for_machine).to eq '/usr/lib64'
+      context 'freebsd' do
+        platform 'freebsd'
+        it { is_expected.to write_log("/usr/local") }
       end
     end
   end
 end
-
-  # describe '#lib_dir' do
-  #   before do
-  #     allow(subject).to receive(:[]).with('platform_family').and_return(platform_family)
-  #     allow(subject).to receive(:[]).with('kernel').and_return(kernel: {machine: 's390x'})
-  #
-  #     # allow(subject).to receive(:[]).and_return(
-  #     #   platform_family
-  #       # kernel: {machine: 'fake'}
-  #     # )
-  #   end
-  #
-  #   context 'with rhel family' do
-  #     let(:platform_family) { 'rhel' }
-  #
-  #     it 'returns the correct directory' do
-  #       expect(subject.lib_dir).to eq '/usr/lib64/httpd'
-  #     end
-  #   end
-  #
-  #   context 'with amazon family' do
-  #     let(:platform_family) { 'amazon' }
-  #
-  #     it 'returns the correct directory' do
-  #       expect(subject.lib_dir).to eq '/usr/lib64/httpd'
-  #     end
-  #   end
-  #
-  #   context 'with SUSE family' do
-  #     let(:platform_family) { 'suse' }
-  #
-  #     it 'returns the correct directory' do
-  #       expect(subject.lib_dir).to eq '/usr/lib64/apache2'
-  #     end
-  #   end
-  #
-  #   context 'with Debian family' do
-  #     let(:platform_family) { 'debian' }
-  #
-  #     it 'returns the correct directory' do
-  #       expect(subject.lib_dir).to eq '/usr/lib/apache2'
-  #     end
-  #   end
-  #
-  #   context 'with Arch family' do
-  #     let(:platform_family) { 'arch' }
-  #
-  #     it 'returns the correct directory' do
-  #       expect(subject.lib_dir).to eq '/usr/lib/httpd'
-  #     end
-  #   end
-  #
-  #   context 'with FreeBSD family' do
-  #     let(:platform_family) { 'frebsd' }
-  #
-  #     it 'returns the correct directory' do
-  #       expect(subject.lib_dir).to eq '/usr/local/libexec/apache24'
-  #     end
-  #   end
-  #
-  #   context 'with unknown platform' do
-  #     let(:platform_family) { 'foobar' }
-  #
-  #     it 'returns the default directory' do
-  #       expect(subject.lib_dir).to eq '/usr/lib/apache2'
-  #     end
-  #   end
-#   end
-# end
