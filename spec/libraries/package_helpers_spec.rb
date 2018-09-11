f34b985d365bb377af7f730a20bc59a365ec311e
Dan Webb <dan.webb@damacus.io>
2018-09-11 09:33:47 +0100

    Add apache_package helper
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- spec/libraries/package_helpers_spec.rb
+++ spec/libraries/package_helpers_spec.rb
@@ -20,6 +20,22 @@ RSpec.describe Apache2::Cookbook::Helpers do
       end
     end
 
+    context 'with fedora family' do
+      let(:platform_family) { 'fedora' }
+
+      it 'returns the correct path' do
+        expect(subject.perl_pkg).to eq 'perl'
+      end
+    end
+
+    context 'with amazon family' do
+      let(:platform_family) { 'amazon' }
+
+      it 'returns the correct path' do
+        expect(subject.perl_pkg).to eq 'perl'
+      end
+    end
+
     context 'with SUSE family' do
       let(:platform_family) { 'suse' }
 
@@ -48,5 +64,53 @@ RSpec.describe Apache2::Cookbook::Helpers do
         expect(subject.perl_pkg).to eq 'perl5'
       end
     end
+  end
+end
+
+describe '#apache_pkg' do
+  recipe do
+    log apache_pkg
+  end
+
+  context 'with rhel family' do
+    platform 'redhat'
+    it { is_expected.to write_log('httpd') }
+  end
+
+  context 'with fedora family' do
+    platform 'fedora'
+    it { is_expected.to write_log('httpd') }
+  end
+
+  context 'with amazon family' do
+    context 'on amazon-1' do
+      platform 'amazon', '2018'
+      it { is_expected.to write_log('httpd24') }
+    end
+
+    context 'on amazon-2' do
+      platform 'amazon', '2'
+      it { is_expected.to write_log('httpd') }
+    end
+  end
+
+  context 'with SUSE family' do
+    platform 'suse'
+    it { is_expected.to write_log('apache2') }
+  end
+
+  context 'with Debian' do
+    platform 'debian'
+    it { is_expected.to write_log('apache2') }
+  end
+
+  context 'with arch' do
+    platform 'arch'
+    it { is_expected.to write_log('apache') }
+  end
+
+  context 'with freebsd' do
+    platform 'freebsd'
+    it { is_expected.to write_log('apache24') }
   end
 end
