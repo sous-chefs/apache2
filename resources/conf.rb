8926772c1869516232620582e0d0af5937b95773
Dan Webb <dan.webb@damacus.io>
2018-10-25 16:01:15 +0100

    Fix some license strings

--- resources/conf.rb
+++ resources/conf.rb
@@ -3,6 +3,7 @@
 # Resource:: apache2_conf
 #
 # Copyright:: 2008-2017, Chef Software, Inc.
+# Copyright:: 2018, Webb Agile Solutions Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- resources/mod_auth_cas.rb
+++ resources/mod_auth_cas.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook:: apache2
+# Resource:: apache2_conf
+#
+# Copyright:: 2008-2017, Chef Software, Inc.
+# Copyright:: 2018, Webb Agile Solutions Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 include Apache2::Cookbook::Helpers
 
 property :install_method, String,
@@ -22,7 +41,7 @@ property :mpm, String,
 
 action :install do
   if new_resource.install_method.eql? 'source'
-    package apache_devel_package(new_resource)
+    package apache_devel_package(new_resource.mpm)
 
     git '/tmp/mod_auth_cas' do
       repository 'git://github.com/Jasig/mod_auth_cas.git'
