e5313527dc040dbb0389fca689ec770b66529a12
Dan Webb <dan.webb@damacus.io>
2018-11-07 17:54:57 +0000

    Add mod_info

---
+++ resources/mod_info.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook:: apache2
+# Resource:: apache2_conf
+#
+# Copyright:: 2008-2017, Chef Software, Inc.
+# Copyright:: 2018, Webb Agile Solutions Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include Apache2::Cookbook::Helpers
+
+property :options, String,
+         default: 'Indexes MultiViews SymLinksIfOwnerMatch',
+         description: ''
+property :icondir, String,
+         default: lazy { icon_dir },
+         description: 'The icon directory'
+property :allow_override, String,
+         default: 'None',
+         description: 'For full description see https://httpd.apache.org/docs/2.4/mod/core.html#allowoverride'
+property :require, String,
+         default: 'all granted',
+         description: 'For full description see https://httpd.apache.org/docs/2.4/mod/mod_authz_core.html#require'
+
+action :create do
+  template ::File.join(apache_dir, 'mods-available', 'mod_info.conf') do
+    source 'mods/info.conf.erb'
+    cookbook 'apache2'
+    variables(
+      icondir: new_resource.icondir,
+      options: new_resource.options,
+      allow_override: new_resource.allow_override,
+      require: new_resource.require
+    )
+  end
+end
