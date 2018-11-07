e00aeccfc7f486d71a55b8beb5055a219a8f6ea3
Dan Webb <dan.webb@damacus.io>
2018-11-07 17:44:32 +0000

    Update config licensing

--- resources/config.rb
+++ resources/config.rb
@@ -1,13 +1,15 @@
 #
-# Author:: Joshua Timberman <joshua@chef.io>
-# Copyright:: Copyright (c) 2012, Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Cookbook:: apache2
+# Resource:: apache2_config
+#
+# Copyright:: 2008-2017, Chef Software, Inc.
+# Copyright:: 2018, Webb Agile Solutions Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,5 +17,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-include_recipe 'apache2::god_monitor'
