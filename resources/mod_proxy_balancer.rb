4133ff011f12a1c8d451eca13b753c87cf080224
Dan Webb <dan.webb@damacus.io>
2018-11-07 17:57:27 +0000

    Add mod_proxy balancer

---
+++ resources/mod_proxy_balancer.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook:: apache2
+# Resource:: apache2_conf
+#
+# Copyright:: 2008-2017, Chef Software, Inc.
+# Copyright:: 2018, Webb Agile Solutions Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include Apache2::Cookbook::Helpers
+
+property :status_location, String,
+         default: '/balancer-manager',
+         description: ''
+property :set_handler, String,
+         default: 'balancer-manager',
+         description: ''
+property :require, String,
+         default: 'local',
+         description: 'For full description see https://httpd.apache.org/docs/2.4/mod/mod_authz_core.html#require'
+
+action :create do
+  template ::File.join(apache_dir, 'mods-available', 'mod_ssl.conf') do
+    source 'mods/alias.conf.erb'
+    cookbook 'apache2'
+    variables(
+      status_location: new_resource.status_location,
+      set_handler: new_resource.set_handler,
+      require: new_resource.require,
+    )
+  end
+end
