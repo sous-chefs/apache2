09c4d7421b6538fa9e31861f338e54ad3db97d18
Dan Webb <dan.webb@damacus.io>
2018-11-15 11:59:51 +0000

    Make security, ports & charset all use the conf resource

--- resources/conf.rb
+++ resources/conf.rb
@@ -29,6 +29,14 @@ property :template_cookbook, String,
          default: 'apache2',
          description: 'Cookbook to source the template from.
          Override this to provide your own template.'
+property :options, Hash,
+         default: {
+           server_tokens: 'Prod',
+           server_signature: 'On',
+           trace_enable: 'Off',
+           listen: '80',
+         },
+         description: 'A hash to pass to the template'
 
 action :enable do
   template ::File.join(new_resource.path, "#{new_resource.name}.conf") do
@@ -38,7 +46,11 @@ action :enable do
     backup false
     mode '0644'
     variables(
-      apache_dir: apache_dir
+      apache_dir: apache_dir,
+      server_tokens: new_resource.options[:server_tokens],
+      server_signature: new_resource.options[:server_signature],
+      trace_enable: new_resource.options[:trace_enable],
+      listen: new_resource.options[:listen]
     )
     notifies :restart, 'service[apache2]', :delayed
   end
