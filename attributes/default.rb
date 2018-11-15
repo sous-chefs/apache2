67b4e7f53af149e2847af8897b8618427a5e698f
Dan Webb <dan.webb@damacus.io>
2018-11-15 16:40:51 +0000

    Remove attributes we no longer use
    Removes openid, access_file_name and other parameters we no longer use

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,6 +24,24 @@
 ###
 
 # General settings
-default['apache']['sysconfig_additional_params'] = {}
 default['apache']['default_site_port'] = '80'
-default['apache']['access_file_name'] = '.htaccess'
+
+# mod_auth_openids
+default['apache']['mod_auth_openid']['ref']        = 'v0.8'
+default['apache']['mod_auth_openid']['version']    = '0.8'
+default['apache']['mod_auth_openid']['source_url'] = "https://github.com/bmuller/mod_auth_openid/archive/#{node['apache']['mod_auth_openid']['ref']}.tar.gz"
+default['apache']['mod_auth_openid']['cache_dir']  = '/var/cache/mod_auth_openid'
+default['apache']['mod_auth_openid']['dblocation'] = "#{node['apache']['mod_auth_openid']['cache_dir']}/mod_auth_openid.db"
+
+default['apache']['mod_auth_openid']['configure_flags'] =
+  case node['platform_family']
+  when 'freebsd'
+    [
+      'CPPFLAGS=-I/usr/local/include',
+      'LDFLAGS=-I/usr/local/lib -lsqlite3',
+    ]
+  else
+    []
+  end
+
+  
