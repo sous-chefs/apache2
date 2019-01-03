a871c2907dead96d50752bf9abb4a67b5aa4022a
Dan Webb <dan.webb@damacus.io>
2019-01-03 09:10:25 +0000

    Remove license headers
    These aren't needed as everything is licensed under Apache-2.0

--- test/cookbooks/test/recipes/mod_ssl.rb
+++ test/cookbooks/test/recipes/mod_ssl.rb
@@ -1,22 +1,3 @@
-#
-# Cookbook:: apache2_test
-# Recipe:: mod_ssl
-#
-# Copyright:: 2012, Chef Software, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 ssl_dir = '/home/apache2'
 ssl_cert_file = "#{ssl_dir}/server.crt"
 ssl_cert_key_file = "#{ssl_dir}/server.key"
