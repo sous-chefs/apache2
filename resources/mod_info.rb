2a17e18646609a247c9e008b77394694a57bfbc6
Benoit Plessis <b.plessis@doyousoft.com>
2019-05-20 21:56:25 +0200

    mod info arguments type consistency with mod-status (#625)
    * Allow array syntax for mod-info's allow-list like mod-status
    
    * Auto-convert the property to an Array when it's a type String
    
    * Add a CHANGELOG entry, and also add the 'string-way' to mod_status

--- resources/mod_info.rb
+++ resources/mod_info.rb
@@ -1,12 +1,12 @@
-property :info_allow_list, String,
-         default: '127.0.0.1 ::1',
+property :info_allow_list, [String, Array],
+         default: %w(127.0.0.1 ::1),
          description: ''
 
 action :create do
   template ::File.join(apache_dir, 'mods-available', 'info.conf') do
     source 'mods/info.conf.erb'
     cookbook 'apache2'
-    variables(info_allow_list: new_resource.info_allow_list)
+    variables(info_allow_list: Array(new_resource.info_allow_list))
   end
 end
 
