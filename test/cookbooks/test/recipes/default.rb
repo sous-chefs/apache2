f8e65e634560984fa902caf4d5848fd708798486
John Roesler <johnrroesler@gmail.com>
2019-06-19 09:21:24 -0500

    fix codeowners to use @ mention

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,3 +1,8 @@
+yum_repository 'codeit' do
+  baseurl "https://repo.codeit.guru/packages/centos/#{node['platform_version'].to_i}/$basearch"
+  gpgkey 'https://repo.codeit.guru/RPM-GPG-KEY-codeit'
+end
+
 apt_update 'update'
 
 apache2_install 'default_install'
