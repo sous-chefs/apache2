11e24d0e3ca6d4c03d12d46780444121b2d3b20e
Jason Field <jason@avon-lea.co.uk>
2019-10-02 08:49:16 +0100

    Fix port tests

--- test/integration/ports/controls/default.rb
+++ test/integration/ports/controls/default.rb
@@ -2,12 +2,12 @@ include_controls 'apache2-integration-tests' do
   skip_control 'welcome-page'
 end
 
-control 'hello-world' do
+control 'responds on 8080' do
   impact 1
-  desc 'Hello World page should be visible on port 8080'
+  desc 'Apache should respond on on port 8080'
 
-  describe http('https://127.0.0.1:8080', ssl_verify: false) do
-    its('status') { should eq 200 }
-    its('body') { should cmp /Hello World/ }
+  describe http('http://127.0.0.1:8080') do
+    its('status') { should eq 403 }
+    its('body') { should cmp /Apache Server/ }
   end
 end
