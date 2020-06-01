b91f548ca2fb70b36d71b56af24ce3b03f66b3b0
Dan Webb <dan.webb@damacus.io>
2020-06-01 14:02:22 +0100

    Same thing with FreeBSD

--- test/integration/default/controls/default_spec.rb
+++ test/integration/default/controls/default_spec.rb
@@ -12,7 +12,6 @@ control 'service' do
   when 'freebsd'
     describe service('apache24') do
       it { should be_installed }
-      it { should be_enabled }
       it { should be_running }
     end
   else
