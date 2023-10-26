f90d4fc81c87eeb45b966f822b946cfa99556e6b
Dan Webb <dan.webb@damacus.io>
2023-10-26 09:50:17 +0100

    Fix Debian tests
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/integration/default/controls/default_spec.rb
+++ test/integration/default/controls/default_spec.rb
@@ -79,9 +79,10 @@ control 'pid file' do
   desc 'PID file should be setup correctly'
   case os[:family]
   when 'debian'
+    # Debian doesn't use /etc/sysconfig instead it relies on /etc/apache2/envvars
     describe file("#{apache_dir}/envvars") do
       it { should exist }
-      its('content') { should cmp 'PIDFILE=/var/run/apache2/apache2.pid' }
+      its('content') { should cmp 'PidFile /var/run/apache2/apache2.pid' }
     end
   when 'redhat'
     describe file("/etc/sysconfig/#{apache_platform_service_name}") do
