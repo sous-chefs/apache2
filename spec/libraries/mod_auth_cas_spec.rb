0472578482a3cf79fd24dac3c8c828f0a8a390f3
Dan Webb <dan.webb@damacus.io>
2026-01-14 12:37:34 +0000

    fix: enable previously excluded CI test suites

--- spec/libraries/mod_auth_cas_spec.rb
+++ spec/libraries/mod_auth_cas_spec.rb
@@ -67,37 +67,37 @@ RSpec.describe Apache2::Cookbook::Helpers do
     context 'redhat 8' do
       let(:platform_family) { 'rhel' }
       let(:platform_version) { '8' }
-      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(openssl-devel libcurl-devel pcre-devel libtool) }
+      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(openssl-devel libcurl-devel pcre2-devel libtool) }
     end
 
     context 'redhat 7' do
       let(:platform_family) { 'rhel' }
       let(:platform_version) { '7' }
-      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(openssl-devel libcurl-devel pcre-devel libtool) }
+      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(openssl-devel libcurl-devel pcre2-devel libtool) }
     end
 
     context 'debian' do
       let(:platform_family) { 'debian' }
       let(:platform_version) { '11' }
-      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(libssl-dev libcurl4-openssl-dev libpcre++-dev libtool) }
+      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(libssl-dev libcurl4-openssl-dev libpcre2-dev libtool) }
     end
 
     context 'ubuntu' do
       let(:platform_family) { 'debian' }
       let(:platform_version) { '22.04' }
-      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(libssl-dev libcurl4-openssl-dev libpcre++-dev libtool) }
+      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(libssl-dev libcurl4-openssl-dev libpcre2-dev libtool) }
     end
 
     context 'amazonlinux' do
       let(:platform_family) { 'amazon' }
       let(:platform_version) { '2023' }
-      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(openssl-devel libcurl-devel pcre-devel libtool) }
+      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(openssl-devel libcurl-devel pcre2-devel libtool) }
     end
 
     context 'suse' do
       let(:platform_family) { 'suse' }
       let(:platform_version) { '15.1' }
-      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(libopenssl-devel libcurl-devel pcre-devel libtool) }
+      it { expect(subject.apache_mod_auth_cas_devel_packages).to eq %w(libopenssl-devel libcurl-devel pcre2-devel libtool) }
     end
   end
 end
